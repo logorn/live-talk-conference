aa60876cdf8a503309aa90c5d78bba32c6b14a70
Martin Gontovnikas <martin@gonto.com.ar>
2013-05-12 14:26:00 -0300

    Fixed problem with app.js

--- config/routes.js
+++ config/routes.js
@@ -2,12 +2,14 @@
 var express = require('express')
   , routes = require('../routes')
   , talk = require('../routes/talk')
+  , chat = require('../routes/chat')
   , path = require('path');
 
 
 
 module.exports = function (app, config) {
   app.get('/', routes.index);
+  app.get('/chat', chat.index);
   app.resource('talks', talk);
 };
   
